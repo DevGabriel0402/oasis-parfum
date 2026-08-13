d52118929dcc65222cbfd6b49d5d019a34dee540
DevGabriel0402 <gabriel.profissional01@gmail.com>
2026-08-13 13:14:39 -0300

    Atualizando

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./styles.css";
-import "./settings.css";
-import "./catalog-theme.css";
-import "./catalog-header.css";
-import "./shadcn-theme.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
